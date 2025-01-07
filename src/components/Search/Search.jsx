79d7eb4944da64a5cd279ba613d989418dab4de2
jane <jconradie1998@gmail.com>
2025-01-07 10:33:26 +0200

    adding debounce + fix styling

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,14 +1,29 @@
 import "./style.css";
 
+import { useDebounce } from "../../hooks/useDebounce";
+import { useEffect, useState } from "react";
+
 const Search = ({ setSearchString }) => {
+  const [searchValue, setSearchValue] = useState("");
+  const searchQuery = useDebounce(searchValue, 500);
+
   const handleSearchChange = (event) => {
-    setSearchString(event.target.value);
+    setSearchValue(event.target.value);
   };
+
+  useEffect(() => {
+    setSearchString(searchValue);
+  }, [searchQuery]);
 
   return (
     <div className={"sorter search"}>
       <h2>Search:</h2>
-      <input className="search-bar" type="text" onChange={handleSearchChange} />
+      <input
+        className="search-bar"
+        type="text"
+        value={searchValue}
+        onChange={handleSearchChange}
+      />
     </div>
   );
 };
